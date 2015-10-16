f4bdcc2a6ca4e326888248da9aa2319051466537
Jekaterina Vorošilova <jekavor@greeny.cs.tlu.ee>
2015-10-16 10:42:13 +0300

    added

--- table.php
+++ table.php
@@ -27,7 +27,24 @@
 	<?php
 	                   //iga massiivis olema elemendi kohta
 					   //count ($car_list) - massiiivi pikkus
-					   for($i = 0; $i < count($car_list); $i++){
+					 for($i = 0; $i < count($car_list); $i++){
+						   
+						if (isset($_GET["edit"]) && $car_list[$i]->id == $GET["edit"]{
+							  //kasutajale muutmiseks
+							 echo"<tr>";
+							   echo "<form action='table.php' method='post'>";
+							   echo"<form>";
+							           echo "<td>". $car_list[$i]->id."</td>";
+						               echo "<td>". $car_list[$i]->user_id."</td>";
+									   echo "<td><input name='number_plate' value='" . $car_list[$i]->number_plate."'></td>";
+									   echo "<td><input name='color' value='" . $car_list[$i]->colour."'></td>";	
+                                       echo "<td><input type='submit', name='update'></td>";
+                                       echo "<td><a href='table.php'>cancel</a></td>";									   
+							   echo"</form>";
+							   echo"<tr>Seda rida muudetakse</tr>";
+							 echo"</tr>";
+					       }else{
+							   //tavaline rida
 						   // $i = $i + 1; sama mis $i= +=1; sama mis $i++;
 						   echo"<tr>";
 						   
@@ -38,7 +55,7 @@
 						   echo "<td><a href='?delete=". $car_list[$i]->id."'>X</a></td>";
 						   
 						   echo"</tr>";
-					   }
+					}
 					   
 	
 	?>
